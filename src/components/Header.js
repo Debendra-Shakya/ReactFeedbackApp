2630c03666147c2170e7c60b4e0b57070246bdf9
debz1 <debenshakya1@gmail.com>
2022-04-29 08:24:08 +0545

    Header component added

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Header({ text, bgcolor }) {
+  const style = {
+    backgroundColor: bgcolor,
+  };
+
+  return (
+    <div style={style}>
+      <h2>{text}</h2>
+    </div>
+  );
+}
+Header.defaultProps = {
+  text: "i am a header default prop!",
+  bgcolor: "hsl(80deg 100% 50%)",
+};
+Header.propTypes = {
+  text: PropTypes.string,
+};
+
+export default Header;
